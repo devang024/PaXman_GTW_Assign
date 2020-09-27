5583c380c21402775ac73a1193a19aab8719bda2
devang024 <devang024@live.co.uk>
2020-09-27 17:08:30 +0530

    -added tile types and floor formations

--- Assets/Scripts/FloorMaker.cs
+++ Assets/Scripts/FloorMaker.cs
@@ -7,11 +7,16 @@ public class FloorMaker : MonoBehaviour
     [Header("Not Less then 30")]
     public int Rows;
 
-    [Header("Not Less then 22")]
+    [Header("Not Less then 22,Should match 30/22=1.36 Aspect Ratio.")]
     public int Columns;
 
     [SerializeField]
     Camera SceneCam;
+
+    public GameObject PrefabTile;
+    public Transform TileParent;
+
+    public FloorTile[,] Grid;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +25,35 @@ public class FloorMaker : MonoBehaviour
         {
             Rows = 30;Columns = 22;
         }
+        Grid = new FloorTile[Rows, Columns];
         this.transform.localScale = new Vector3(Rows,Columns,this.transform.localScale.z);
-        SceneCam.orthographicSize = (Columns / 2);
-        SceneCam.transform.position = new Vector3(Rows/2,Columns/2,SceneCam.transform.position.z);
 
-        
+        float offset = (((float)Columns) * (4.5f / 100.0f));//We offset the height of Grid by 5% of Columns number
+        SceneCam.orthographicSize = (Columns / 2) + offset;
 
+        SceneCam.transform.position = new Vector3((float)Rows*0.5f, (float)Columns*0.5f ,SceneCam.transform.position.z);
+        PopulateTiles();
+    }
+
+    void PopulateTiles()
+    {
+        GameObject _tempObj;
+        for( int Xindex=0;Xindex<Rows;Xindex++ )
+        {
+            for(int Yindex=0;Yindex<Columns;Yindex++)
+            {
+                _tempObj = Instantiate(PrefabTile,TileParent) as GameObject;
+                if( _tempObj != null )
+                {
+                    Grid[Xindex, Yindex] = _tempObj.GetComponent<FloorTile>();
+                    _tempObj.transform.position = new Vector3(Xindex,Yindex,_tempObj.transform.position.z);
+                    if ( Xindex == 0 || Yindex==0 || Xindex==Rows-1 || Yindex==Columns-1 )
+                    {
+                        Grid[Xindex, Yindex].TileTypeGetSet = FloorTile.TileType.Boundary;
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/FloorTile.cs
+++ Assets/Scripts/FloorTile.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class FloorTile : MonoBehaviour
 {
+
+    public enum TileType
+    {
+        Boundary,
+        Fixed,
+        Tentative,
+        Space
+    }
+
+    TileType _tileType = TileType.Space;
+
+    public TileType TileTypeGetSet
+    {
+        get
+        {
+            return this._tileType;
+        }
+
+        set
+        {
+            switch( value )
+            {
+                case TileType.Boundary:
+                    GetComponent<SpriteRenderer>().color = Color.white;
+                    break;
+            }
+            this._tileType = value;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +45,7 @@ public class FloorTile : MonoBehaviour
     {
         
     }
+
+    
+
 }
